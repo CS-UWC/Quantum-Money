b3e64539195ac145ed15209f263b7e3de4a31322
IsaTippens <isatippens2@gmail.com>
2023-10-29 22:55:30 +0200

    Old qnote

--- quantum/qnote.py
+++ quantum/qnote.py
@@ -1,3 +1,9 @@
+
+"""
+    This class is unused
+
+    model.QuantumNote is used instead
+"""
 class QNote:
     def __init__(self, serial: str, states: list[int] = []):
         self.serial = serial
